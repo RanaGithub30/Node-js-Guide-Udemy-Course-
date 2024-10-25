bf434be54e9efff7a417d2a9d52328cc01a81c4b
Rana Saha <ranasaha03edu@gmail.com>
2024-10-25 23:21:22 +0530

    views setup

--- Projects/ecommerce/controllers/frontController.js
+++ Projects/ecommerce/controllers/frontController.js
@@ -1,3 +1,19 @@
 module.exports.index = (req, res, next) => {
     res.render('front/index', { title: 'Home' });
 };
+
+module.exports.shop = (req, res, next) => {
+    res.render('front/shop', {title: 'Shop'});
+};
+
+module.exports.whyUs = (req, res, next) => {
+    res.render('front/why-us', {title: 'Why Us'});
+};
+
+module.exports.testimonial = (req, res, next) => {
+    res.render('front/testimonial', {title: 'Testimonial'});
+};
+
+module.exports.contact = (req, res, next) => {
+    res.render('front/contact', {title: 'Contact'});
+};
--- Projects/ecommerce/routes/front.js
+++ Projects/ecommerce/routes/front.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const frontController = require('../controllers/frontController');
 
 router.get('/', frontController.index);
+router.get('/shop', frontController.shop);
+router.get('/why-us', frontController.whyUs);
+router.get('/testimonial', frontController.testimonial);
+router.get('/contact-us', frontController.contact);
 
 module.exports = router;
