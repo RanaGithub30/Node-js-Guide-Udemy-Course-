3f79eb793248c5b05ea6cd6b5f26872c9f35b181
Rana Saha <ranasaha03edu@gmail.com>
2024-11-14 23:33:28 +0530

    Session Remove functionality done

--- session-cookie/routes/auth.js
+++ session-cookie/routes/auth.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/auth');
 
 router.get('/login', authController.login);
 router.post('/login/submit', authController.postLogin);
+router.get('/logout', authController.logout);
 
 module.exports = router;
