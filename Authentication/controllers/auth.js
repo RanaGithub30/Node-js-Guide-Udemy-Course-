f4019f220b8e246bfdf9a18cd86c15d0ea02a196
Rana Saha <ranasaha03edu@gmail.com>
2024-11-20 23:25:13 +0530

    Add flush message

--- Authentication/controllers/auth.js
+++ Authentication/controllers/auth.js
@@ -2,12 +2,19 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if(message.length > 0){
+      message = message[0]
+  }else{
+      message = null;
+  }
+
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
     isAuthenticated: false,
     csrfToken: req.csrfToken(),
-    errorMessage: req.flash('error')
+    errorMessage: message
   });
 };
 
