331c8965be9a531f9fd72cdcc38a9b19b798ffd8
Rana Saha <ranasaha03edu@gmail.com>
2024-11-28 18:15:31 +0530

    Add Basic Validation

--- Validation/controllers/auth.js
+++ Validation/controllers/auth.js
@@ -2,6 +2,7 @@ const crypto = require('crypto'); // a builtin libtrary to help us generate a se
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
+const { validationResult } = require('express-validator');
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -67,6 +68,17 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    console.log(errors.array());
+      return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      isAuthenticated: false,
+      csrfToken: req.csrfToken(),
+      errorMessage: errors.array()
+    });
+  }
 
   User.findOne({email: email})
   .then(userDoc => {
