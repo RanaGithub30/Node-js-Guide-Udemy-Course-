cc6cd21df13835b3021792adb7cb7550c12893cb
Rana Saha <ranasaha03edu@gmail.com>
2024-12-15 23:34:44 +0530

    Setup Checkout Page

--- Payment-Gateway/controllers/shop.js
+++ Payment-Gateway/controllers/shop.js
@@ -201,3 +201,26 @@ exports.getInvoice = (req, res, next) => {
         pdfDoc.end();
     }).catch(err => next(err));
 }
+
+/** Checkout */
+
+exports.getCheckout = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach(p => {
+          total += p.quantity * p.productId.price;
+      });
+
+      res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout',
+        products: products,
+        totalSum: total
+      });
+    })
+    .catch(err => console.log(err));
+}
--- Payment-Gateway/routes/shop.js
+++ Payment-Gateway/routes/shop.js
@@ -14,5 +14,6 @@ router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 router.post('/create-order', isAuth, shopController.postOrder);
 router.get('/orders', isAuth, shopController.getOrders);
 router.get('/invoice/:orderId', isAuth, shopController.getInvoice);
+router.get('/checkout', isAuth, shopController.getCheckout);
 
 module.exports = router;
